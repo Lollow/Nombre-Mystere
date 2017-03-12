d48b88128de7b62b4562f5b5c19cac460cdb2614
Loraine Perendevy <perendevy.loraine@gmail.com>
2017-03-12 11:41:15 +0100

    Les conditions

--- js/app.js
+++ js/app.js
@@ -2,4 +2,12 @@ var solution = Math.round (Math.random() * (3-0) +0);
 console.log(solution);
 
 var response = prompt("Choississez un nombre !");
-console.log("response");
+console.log(response);
+
+if (response == solution){
+	console.log("Bonne réponse !");
+}else if (response > solution){
+	console.log("Nombre trop grand !");
+}else{
+	console.log("Nombre trop petit !");
+}
